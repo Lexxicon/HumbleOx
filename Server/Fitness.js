8525f1ab0a98bb70881fc7e2b452ef3210eb7bfd
bholman <dolorosul@gmail.com>
2017-05-30 08:57:53 -0500

    made fitness account for speed a little bit

--- Server/Fitness.js
+++ Server/Fitness.js
@@ -6,7 +6,7 @@ Fitness.prototype.evaluate = function(creep, target){
   let dst = creep.pos.distance(target);
   let startDst = creep.spawnLocation.distance(target);
   if(dst < 1){
-    return 1;
+    return 1 + (creep.timeToLive - creep.age) / creep.timeToLive;
   }else{
     return (startDst - dst).startDst;
   }
